f75f842f54a208b24e72a4cb24bedc107e63f75e
Luis Leal <luisrrleal@gmail.com>
2023-01-12 20:54:49 -0600

    Add material UI and themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
+import { customTheme, darkTheme } from '../themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
