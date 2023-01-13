731bc40d55517d48c4f3110218963a60fac97e9a
Luis Leal <luisrrleal@gmail.com>
2023-01-12 21:21:53 -0600

    Add layout and aditional pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Layout } from '../components/Layout';
 
 export default function Home() {
   return (
-    <>
-  <h1>Cookie Master</h1>
-    </>
+    <Layout>
+      <h1>Cookie Master</h1>
+    </Layout>
   )
 }
---
+++ pages/theme-changer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout } from '../components/Layout';
+
+const themeChanger = () => {
+    return (
+        <Layout>
+            <h1>Theme Changer</h1>
+        </Layout>
+    )
+}
+
+export default themeChanger;
